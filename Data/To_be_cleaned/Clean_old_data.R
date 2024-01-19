75120aa15856d5f9f233cf47e9ac310cc09cda87
Ben Makhlouf <makhlobe@gmail.com>
2024-01-18 19:25:04 -0800

    successfully exported 2010 Yukon to Natal Origin

--- Data/To_be_cleaned/Clean_old_data.R
+++ Data/To_be_cleaned/Clean_old_data.R
@@ -5,7 +5,7 @@ library(lubridate)
 
 
 ### 2010 Yukon 
-Yukon2010<- read_csv(here("Data/To_be_cleaned/Natal Origin/2010 Yukon_natal_data.csv"))
+Yukon2010<- read_csv(here("Data/Natal Origin/2010 Yukon_natal_data.csv"))
 
 #Take the value in the column file.id, and remove _yukon_chinook_
 Yukon2010$fish.id <- gsub("_yukon_chin_", "", Yukon2010$file.id)
@@ -19,3 +19,6 @@ Yukon2010$capture_date <- as.Date(Yukon2010$capture_date, format = "%m/%d/%y")
 # Convert capture_date to day of the year (Julian day)
 Yukon2010$capture_date_julian<- yday(as.Date(Yukon2010$capture_date, format = "%m/%d/%y"))
 
+
+#Write to a csv file
+write_csv(Yukon2010, here("Data/Natal Origin/2010 Yukon_natal_data.csv"))
