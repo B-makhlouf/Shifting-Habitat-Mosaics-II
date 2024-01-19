193a095f69aef40815ae7b1bcaf81da8cf5239e9
Ben Makhlouf <makhlobe@gmail.com>
2024-01-18 19:21:47 -0800

    finally got Julian date to work

--- Data/To_be_cleaned/Clean_old_data.R
+++ Data/To_be_cleaned/Clean_old_data.R
@@ -1,8 +1,10 @@
 library(here)
 library(dplyr)
 library(readr)
+library(lubridate)
 
 
+### 2010 Yukon 
 Yukon2010<- read_csv(here("Data/To_be_cleaned/Natal Origin/2010 Yukon_natal_data.csv"))
 
 #Take the value in the column file.id, and remove _yukon_chinook_
@@ -10,3 +12,10 @@ Yukon2010$fish.id <- gsub("_yukon_chin_", "", Yukon2010$file.id)
 
 #Add a space to file.id in the 4th and 7th position
 Yukon2010$fish.id <- gsub("yk", "_yk_", Yukon2010$fish.id)
+
+# change the capture date to Julian date
+Yukon2010$capture_date <- as.Date(Yukon2010$capture_date, format = "%m/%d/%y")
+
+# Convert capture_date to day of the year (Julian day)
+Yukon2010$capture_date_julian<- yday(as.Date(Yukon2010$capture_date, format = "%m/%d/%y"))
+
