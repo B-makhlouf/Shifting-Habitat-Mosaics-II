4e50bebb678eede16eea00666f464d322ccb2645
Ben Makhlouf <makhlobe@gmail.com>
2024-01-23 12:50:52 -0800

    wrote a script for creating CPUE weights from given CPUE documents, produced for 2010 successfully

--- Code/CPUE weights.R
+++ Code/CPUE weights.R
@@ -1,4 +1,6 @@
-##----- CPUE ----- DO NOT HAVE YET --------------------------------------------------------------
+##----- CPUE ----- 
+
+
 # Create otoCatch, which groups all the values in natal_values by the 
 # day of the year that they were caught, summarizes the number of 
 # fish caught on that day of the year 
@@ -10,79 +12,34 @@ library(here)
 
 #### Yukon ######## 
 
-## 2019 Yukon 
-
-natal_values<- read_csv(here("data/natal data/2019 Yukon_natal_data.csv"))
-metadata <- read_csv(here("data/metadata/2019 Yukon metadata.csv"))
-CPUE <- read_csv(here("data/CPUE/2019 Yukon CPUE.csv"))
-identifier<- "2019 Yukon"
-
-## 2021 Yukon
-
-natal_values<- read_csv(here("data/natal data/2021 Yukon_natal_data.csv"))
-metadata <- read_csv(here("data/metadata/2021 Yukon metadata.csv"))
-CPUE <- read_csv(here("data/CPUE/2021 Yukon CPUE.csv"))
-identifier<- "2021 Yukon"
-
-############################
-
-#### Kusko ########
-
-## 2019 Kusko 
-natal_values<- read_csv(here("data/natal data/2019 Kusko_natal_data.csv"))
-metadata <- read_csv(here("data/CPUE/2019 Kusko CPUE.csv"))
-CPUE <- read_csv(here("data/CPUE/2019 Kusko CPUE.csv"))
-identifier<- "2019 Kusko"
-
-## 2020 Kusko 
-natal_values<- read_csv(here("data/natal data/2020 Kusko_natal_data.csv"))
-metadata <- NA
-CPUE <- read_csv(here("data/CPUE/2020 Kusko CPUE.csv"))
-identifier<- "2020 Kusko"
-
-## 2021 Kusko 
-natal_values<- read_csv(here("data/natal data/2021 Kusko_natal_data.csv"))
-metadata <- read_csv(here("data/metadata/2021 Yukon metadata.csv"))
-CPUE <- read_csv(here("data/CPUE/2021 Kusko CPUE.csv"))
-identifier<- "2021 Kusko"
+#2010 Yukon 
+natal_values<- read_csv(here("Data/Natal Origin/2010 Yukon_natal_data.csv"))
+CPUE <- read_csv(here("Data/CPUE/Yukon CPUE 2010.csv"))
+identifier <- "2010 Yukon"
 
 # ------------------------------------------------------------------------------
 ## Summarizes the # of fish caught on that day of the year.
+
 otoCatch <- natal_values %>%
   rename(DOY = capture_date_julian)%>%
   group_by(DOY) %>%
   summarise(Catch = length(fish.id))
 
-# Create CPUE, which is the daily catch divided by the sum of the daily catch
-CPUE$`Date Kog.`<- as.Date(CPUE$`Date Kog.`, format = "%m/%d")
-CPUE$`Date Kog.` <- sub("^.{4}", CPUE$`Sample Year`, CPUE$`Date Kog.`)
+CPUE <- CPUE %>%
+  mutate(DOY = format(as.POSIXct(Date), '%j')) %>%#create DOY collumn which makes date into day of year
+  mutate(dailyPROP = dailyCPUE/sum(dailyCPUE)) #calculate the daily proportion of the total catch
+
+CPUE$otoCATCH <- 0 #Create an otocatch collumn in CPUE
+
+matching_indices <- match(otoCatch$DOY, CPUE$DOY) %>% na.omit() # Match DOY values between otoCatch and CPUE data frames
+
+CPUE$otoCATCH[matching_indices] <- otoCatch$Catch # Update the otoCATCH column in CPUE where matches are found
 
 CPUE <- CPUE %>%
-  rename(Date = `Date Kog.`) %>%
-  filter(!is.na(CPUE)) %>%
-  filter(CPUE > 0)
-  
-CPUE <- CPUE %>%
-  mutate(DOY = format(as.POSIXct(Date), '%j'))%>%
-  mutate(dailyPROP = CPUE / sum(CPUE), # How much proportionally is caught ON that day 
-         cumPROP = cumsum(dailyPROP)) # cumulative proportion (how much of the total catch has been caught UP TO that day)
-
-#Create an otocatch collumn in CPUE
-CPUE$otoCATCH <- 0
-
-# Match DOY values between otoCatch and CPUE data frames
-matching_indices <- match(otoCatch$DOY, CPUE$DOY) %>% na.omit()
-
-# Update the otoCATCH column in CPUE where matches are found
-CPUE$otoCATCH[matching_indices] <- otoCatch$Catch
-
-
-CPUE <- CPUE %>%
- filter(otoCATCH > 0) %>%
- mutate(otoPROP = otoCATCH / sum(otoCATCH),
- oto_cumPROP = cumsum(otoPROP),        
- COratio = dailyPROP / otoPROP)        
-
+ filter(otoCATCH > 0) %>% #keep only those with at least 1 fish caught
+ mutate(otoPROP = otoCATCH / sum(otoCATCH), #calculate the otolith proportion
+ oto_cumPROP = cumsum(otoPROP), #calculate the cumulative proportion    
+ COratio = dailyPROP / otoPROP) #calculate the ratio of daily catch to otolith catch    
 
 #------ Creating strata for CPUE weighting -------------------------------------
 
@@ -91,7 +48,6 @@ strat <- rep(NA, nrow(CPUE))
 strat_index <- rep(1:(nrow(CPUE)/NdaysPerStrat), each = NdaysPerStrat)
 strat[is.na(strat)] <- strat_index[is.na(strat)]
 strat[is.na(strat)] <- max(strat, na.rm = TRUE)  # Assign any stragglers to the last stratum
-
 
 #Empty vector for strata wieghts 
 strat.wt <- rep(NA, length(unique(strat)))
@@ -108,24 +64,22 @@ strat.wt <- strat.wt / sum(strat.wt)
 
 # Calculate catch by strata variable
 CatchByStrat <- CPUE %>%
-  mutate(Strat = strat) %>%
-  group_by(Strat) %>%
-  summarise(Nfish = sum(otoCATCH))
+  mutate(Strat = strat) %>% #create a strate collumn
+  group_by(Strat) %>% #group by strata
+  summarise(Nfish = sum(otoCATCH)) #summarize the number of fish caught in each strata
 
 weighting_vector <- rep(NA, nrow(natal_values))
 
 for (i in 1:nrow(natal_values)) {
-DOY<-natal_values$capture_date_julian[i]
-f.strata <- as.numeric(strat[which(CPUE$DOY == DOY)])
-ind_strata_weight<- strat.wt[f.strata]
-
-
-if (length(ind_strata_weight) > 0) {
- weighting_vector[i] <- ind_strata_weight
-} else {
-  weighting_vector[i] <- min(strat.wt, na.rm = TRUE)
-}
-
+  DOY<-natal_values$capture_date_julian[i]
+  f.strata <- as.numeric(strat[which(CPUE$DOY == DOY)])
+  ind_strata_weight<- strat.wt[f.strata]
+  
+  if (length(ind_strata_weight) > 0) {
+    weighting_vector[i] <- ind_strata_weight
+  } else {
+    weighting_vector[i] <- min(strat.wt, na.rm = TRUE)
+  }
 }
 
 weighting_df<- as.data.frame(weighting_vector)
