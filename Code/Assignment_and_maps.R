b37300f7d6ef64e632fbda057c93779c5bf4023c
Ben Makhlouf <makhlobe@gmail.com>
2024-01-28 17:24:28 -0800

    wrote code to create a figure which shows among year variation as compared the sensitivity threshold used

--- Code/Assignment_and_maps.R
+++ Code/Assignment_and_maps.R
@@ -21,6 +21,7 @@ for (year in years){
   }
 }
 
+
 for (year in years){
   for (threshold in sensitivity_thresholds){
       # Run the function
@@ -47,8 +48,14 @@ calculate_metrics <- function(threshold) {
   prod_all$mean <- apply(prod_all, 1, mean)
   prod_all$cv <- prod_all$sd / prod_all$mean
   
-  # Calculate the mean CV for each year
-  prod_all$mean_cv <- mean(prod_all$cv)
+  # Calculate the mean CV for each year without NAs
+  prod_all$mean_cv <- apply(prod_all, 1, mean, na.rm = TRUE)
+  
+  # Calculate change from 2016 to 2017
+  prod_all$change_2016_2017 <- prod_all$year_2017 - prod_all$year_2016
+  
+  # Calculate change from 2015 to 2016 
+  prod_all$change_2015_2016 <- prod_all$year_2016 - prod_all$year_2015
   
   # Export as a .csv
   write_csv(prod_all, here(paste0("Results/Variability/Yukon_Production_all_", threshold, ".csv")))
@@ -57,6 +64,33 @@ calculate_metrics <- function(threshold) {
 for (threshold in sensitivity_thresholds ){
   calculate_metrics(threshold)
 }
+
+
+# Create a dataframe with the collumns threshold and mean_CV
+
+CV_data <- data.frame(
+  threshold = NA,
+  mean_cv = NA
+)
+
+#Loop through each threshold file and add the mean CV to the dataframe
+
+for (threshold in sensitivity_thresholds){
+  CV_data <- CV_data %>% add_row(threshold = threshold, mean_cv = mean(read_csv(here(paste0("Results/Variability/Yukon_Production_all_", threshold, ".csv"))) %>% select(mean_cv) %>% unlist()))
+}
+
+#Plot mean CV vs threshold
+
+ggplot(CV_data, aes(x = threshold, y = mean_cv)) +
+  geom_point(color = "coral") +
+  geom_line(color = "coral") +
+  labs(x = "Threshold", y = "CV")+ 
+  ggtitle("CV of relative production among years vs cutoff threshold")+ 
+  theme_gray()
+
+# Export to figures
+ggsave(here("Results/Figures/Yukon_CV_threshold.png"), width = 6, height = 6, units = "in", dpi = 300)
+
 
 
 
--- Code/CV_maps.R
+++ Code/CV_maps.R
@@ -3,17 +3,21 @@
 library(classInt)
 library(RColorBrewer)
 library(sf)
+library(readr)
+library(here)
 
 yuk_edges<- st_read("/Users/benjaminmakhlouf/Desktop/Research/isoscapes_new/Yukon/UpdatedSSN_20190410/Results/yukon_edges_20191011_2015earlyStrata_acc.shp")
 basin<- st_read("/Users/benjaminmakhlouf/Desktop/Research/isoscapes_new/Yukon/For_Sean/Yuk_Mrg_final_alb.shp")
 StreamOrderPrior <- as.numeric(yuk_edges$Str_Order > 2)
 pid_prior <- yuk_edges$PriorSl2 #Habitat prior ( RCA slope)
-
+threshold<- .5
 
 generate_cv_plot <- function(threshold) {
   # Read in production data with CV metrics
   CV_data <- read_csv(here("Results/Variability", paste0("Yukon_Production_all_", threshold, ".csv")))
   
+  # Add a value of 0 to CV for the bottom 70% of means
+  CV_data$cv[CV_data$mean < median(CV_data$mean)] <- 0
   
   # Extract the variation vector and handle NAs
   variation <- CV_data$cv
@@ -55,3 +59,52 @@ thresholds <- c("0.4","0.5", "0.6", "0.7", "0.8", "0.9" )
 for (threshold in thresholds) {
   generate_cv_plot(threshold)
 }
+
+
+
+
+generate_change_plot <- function(threshold) {
+  # Read in production data with CV metrics
+  CV_data <- read_csv(here("Results/Variability", paste0("Yukon_Production_all_", threshold, ".csv")))
+  
+  # Extract the variation vector and handle NAs
+  change <- CV_data$change_2016_2017
+  change[is.na(change)] <- 0
+  
+  # Calculate breaks
+  breaks <- seq(min(change), max(change), length = 9)
+  if (max(change) > max(change)) {
+    breaks <- seq(min(change[change > 0]), max(change), length = 10)
+  }
+  
+  # Number of colors
+  nclr <- length(breaks)
+  
+  # Filepath for saving the plot
+  filename <- paste0("change_16_17", threshold, "_.pdf")
+  filepath <- file.path(here("Figures", "Maps", "Yukon", filename))
+  
+  # Plotting setup
+  pdf(file = filepath, width = 9, height = 6)
+  plotvar <- change
+  class <- classIntervals(plotvar, nclr, style = "fixed", fixedBreaks = breaks, dataPrecision = 2)
+  plotclr <- rev(brewer.pal(nclr, "RdBu"))
+  colcode <- findColours(class, plotclr, digits = 2)
+  colcode[plotvar == 0] <- 'gray80'
+  
+  # Additional code for the plot (assuming basin, yuk_edges, StreamOrderPrior, and pid_prior are defined)
+  colcode[which(StreamOrderPrior == 0)] <- 'gray60'
+  colcode[which(pid_prior == 0)] <- 'gray60'
+  plot(st_geometry(basin), col = 'gray60', border = 'gray48', main = threshold)
+  plot(st_geometry(yuk_edges), col = colcode, pch = 16, axes = FALSE, add = TRUE, lwd = ifelse(plotvar == 0, 0.05, .6 * (exp(plotvar) - 1)))
+  
+  # Close the plot device
+  dev.off()
+}
+
+
+for (threshold in thresholds) {
+  generate_change_plot(threshold)
+}
+
+
