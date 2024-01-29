165fe3886db6d2fa196dfbd0c163d355dd031e65
Ben Makhlouf <makhlobe@gmail.com>
2024-01-29 12:54:25 -0800

    started thinking about how I could visualize the spread of values differently

--- Code/Explore Variation.R
+++ Code/Explore Variation.R
@@ -1,6 +1,8 @@
 
 #### This script uses the CV and change function to explore how production has varied at each location over time
 library(here)
+library(tidyverse)
+
 
 source(here("Code/Functions/CV_and change_function.R"))
 
@@ -42,3 +44,42 @@ ggplot(CV_data, aes(x = threshold, y = mean_cv)) +
 
 # Export to figures
 ggsave(here("Results/Figures/Yukon_CV_threshold.png"), width = 6, height = 6, units = "in", dpi = 300)
+
+
+
+
+
+
+############# Variation within a year, among thresholds ###### 
+
+full_2015_.4<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.4_basin_norm.csv")) %>% select(basin_assign_rescale)
+full_2015_.5<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.5_basin_norm.csv")) %>% select(basin_assign_rescale)
+full_2015_.6<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.6_basin_norm.csv")) %>% select(basin_assign_rescale)
+full_2015_.7<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.7_basin_norm.csv")) %>% select(basin_assign_rescale)
+full_2015_.8<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.8_basin_norm.csv")) %>% select(basin_assign_rescale)
+full_2015_.9<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.9_basin_norm.csv")) %>% select(basin_assign_rescale)
+
+
+#Put all of these into one dataframe 
+full_2015 <- data.frame(
+  threshold_4 = full_2015_.4$basin_assign_rescale,
+  threshold_5 = full_2015_.5$basin_assign_rescale,
+  threshold_6 = full_2015_.6$basin_assign_rescale,
+  threshold_7 = full_2015_.7$basin_assign_rescale,
+  threshold_8 = full_2015_.8$basin_assign_rescale,
+  threshold_9 = full_2015_.9$basin_assign_rescale
+)
+
+#Put the data into long format
+
+full_2015_long <- full_2015 %>% 
+  pivot_longer(cols = everything(), names_to = "threshold", values_to = "production") %>%
+  arrange(threshold)
+
+#Create a boxplotfor each threshold
+library(ggridges)
+ggplot(full_2015_long, aes(x = production, y = threshold ,color = threshold, fill = threshold)) +
+  geom_density_ridges(alpha = .15)
+
+
+ 
