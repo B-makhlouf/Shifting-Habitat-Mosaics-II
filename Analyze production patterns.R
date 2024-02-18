8b4cf4d5d1eb7a9c2630292350513e51a5c512ea
Ben Makhlouf <makhlobe@gmail.com>
2024-02-18 15:10:39 -0800

    added raw numbers to calculate cv by tributary by stream order

--- Analyze production patterns.R
+++ Analyze production patterns.R
@@ -1,8 +1,10 @@
 # This is a script to analyze patterns of production 
 library(sf)
+library(tidyverse)
+library(here)
 
 # Read in clean shapefiles 
-yuk_edges<-st_read("/Users/benjaminmakhlouf/Desktop/Clean_shapefiles/Yukon_cleaned.shp")
+yuk_edges<-st_read("/Users/benjaminmakhlouf/Desktop/Clean_shapefiles/Yukon_w.tribnames.shp")
 
 ## Read in all the data for the full years of the Yukon 
 yuk_2015_all<- read_csv(here("Data/Production/Yukon/2015_full_Yukon_0.5_basin_norm.csv")) 
@@ -12,165 +14,145 @@ yuk_2019_all<- read_csv(here("Data/Production/Yukon/2019_full_Yukon_0.5_basin_no
 yuk_2021_all<- read_csv(here("Data/Production/Yukon/2021_full_Yukon_0.5_basin_norm.csv"))
 
 #Add stream order values to production estimates based on the indices of edges
-yuk_2015_all$stream_order<- yuk_edges$Str_Order
-yuk_2016_all$stream_order<- yuk_edges$Str_Order
-yuk_2017_all$stream_order<- yuk_edges$Str_Order
-yuk_2019_all$stream_order<- yuk_edges$Str_Order
-yuk_2021_all$stream_order<- yuk_edges$Str_Order
+yuk_2015_all$stream_order<- yuk_edges$Str_Ord
+yuk_2016_all$stream_order<- yuk_edges$Str_Ord
+yuk_2017_all$stream_order<- yuk_edges$Str_Ord
+yuk_2019_all$stream_order<- yuk_edges$Str_Ord
+yuk_2021_all$stream_order<- yuk_edges$Str_Ord
 
 #Add Stream Length values 
-yuk_2015_all$stream_length<- yuk_edges$Shape_Leng
-yuk_2016_all$stream_length<- yuk_edges$Shape_Leng
-yuk_2017_all$stream_length<- yuk_edges$Shape_Leng
-yuk_2019_all$stream_length<- yuk_edges$Shape_Leng
-yuk_2021_all$stream_length<- yuk_edges$Shape_Leng
+yuk_2015_all$stream_length<- yuk_edges$Shp_Lng
+yuk_2016_all$stream_length<- yuk_edges$Shp_Lng
+yuk_2017_all$stream_length<- yuk_edges$Shp_Lng
+yuk_2019_all$stream_length<- yuk_edges$Shp_Lng
+yuk_2021_all$stream_length<- yuk_edges$Shp_Lng
 
-#Add a proportional stream length collumn 
-yuk_2015_all$prop_length<- yuk_2015_all$stream_length/sum(yuk_2015_all$stream_length)
-yuk_2016_all$prop_length<- yuk_2016_all$stream_length/sum(yuk_2016_all$stream_length)
-yuk_2017_all$prop_length<- yuk_2017_all$stream_length/sum(yuk_2017_all$stream_length)
-yuk_2019_all$prop_length<- yuk_2019_all$stream_length/sum(yuk_2019_all$stream_length)
-yuk_2021_all$prop_length<- yuk_2021_all$stream_length/sum(yuk_2021_all$stream_length)
+#Add Tributary names 
+yuk_2015_all$tribs<-yuk_edges$trbtry_
+yuk_2016_all$tribs<-yuk_edges$trbtry_
+yuk_2017_all$tribs<-yuk_edges$trbtry_
+yuk_2019_all$tribs<-yuk_edges$trbtry_
+yuk_2021_all$tribs<-yuk_edges$trbtry_
 
+#Add Year 
+yuk_2015_all$Year<-2015
+yuk_2016_all$Year<-2016
+yuk_2017_all$Year<-2017
+yuk_2019_all$Year<-2019
+yuk_2021_all$Year<-2021
 
-#Combine all
-yuk_all<- rbind(yuk_2015_all, yuk_2016_all, yuk_2017_all, yuk_2019_all, yuk_2021_all)
+#Combine all the years into one dataframe
+yuk_all<-rbind(yuk_2015_all,yuk_2016_all,yuk_2017_all,yuk_2019_all,yuk_2021_all)
 
-high_yuk_all <- yuk_all %>%
-  filter(normalized > .8)
+#Combine across years 
+yuk_across_years<- data_frame(tributary = yuk_2015_all$tribs, 
+                              Stream_order = yuk_2015_all$stream_order,
+                              stream_length = yuk_2015_all$stream_length,
+                              year_2015 = yuk_2015_all$rescaled,
+                              year_2016 = yuk_2016_all$rescaled,
+                              yuk_2021 = yuk_2021_all$rescaled,
+                              avg_mean = (yuk_2015_all$rescaled + yuk_2016_all$rescaled +  yuk_2021_all$rescaled)/5)
 
-#Plot production vs proportional stream length 
-ggplot(high_yuk_all, aes(x = prop_length, y = normalized, color = factor(stream_order))) +
-  geom_point() +
-  labs(title = "Production vs Proportional Stream Length in Yukon",
-       x = "Proportional Stream Length",
-       y = "Production",
-       color = "Stream Order") +
-  theme_minimal() +
-  theme(legend.position = "none")
+#Add sd 
+yuk_across_years <- yuk_across_years %>%
+  rowwise() %>%
+  mutate(sd = sd(c(year_2015, year_2016, yuk_2021)))
 
-#############
-
-#Summarize rescaled by Stream Order 
-StrO_yuk_2015<- yuk_2015_all %>% 
-  group_by(stream_order) %>% 
-  summarize(summed_production = sum(rescaled),
-            summed_prop_length = sum(prop_length)) %>%
-  mutate(year = 2015)
-
-StrO_yuk_2016<- yuk_2016_all %>%
-  group_by(stream_order) %>% 
-  summarize(summed_production = sum(rescaled),
-            summed_prop_length = sum(prop_length)) %>%
-  mutate(year = 2016)
-
-StrO_yuk_2017<- yuk_2017_all %>%
-  group_by(stream_order) %>% 
-  summarize(summed_production = sum(rescaled),
-            summed_prop_length = sum(prop_length)) %>%
-  mutate(year = 2017)
-
-StrO_yuk_2019<- yuk_2019_all %>%
-  group_by(stream_order) %>% 
-  summarize(summed_production = sum(rescaled),
-            summed_prop_length = sum(prop_length)) %>%
-  mutate(year = 2019)
-
-StrO_yuk_2021<- yuk_2021_all %>%
-  group_by(stream_order) %>% 
-  summarize(summed_production = sum(rescaled),
-            summed_prop_length = sum(prop_length)) %>%
-  mutate(year = 2021)
+# Calculate the coefficient of variation for each row 
+yuk_across_years <- yuk_across_years %>%
+  rowwise() %>%
+  mutate(cv = sd/avg_mean)
 
 
-#combine 2015,2016, and 2021
-StrO_yuk_all<- rbind(StrO_yuk_2015, StrO_yuk_2016, StrO_yuk_2021)
+# rescale all the production estimates to be between 0 and 1
+yuk_across_years$rescaled <- (yuk_across_years$avg_mean - min(yuk_across_years$avg_mean)) / (max(yuk_across_years$avg_mean) - min(yuk_across_years$avg_mean))
 
-StrO_yuk_all<- StrO_yuk_all %>% 
-  mutate(dif_prop_length = summed_production - summed_prop_length)
+###### production estimates 
 
-# Plotting
-ggplot(StrO_yuk_all, aes(x = factor(year), y = summed_production, fill = factor(stream_order))) +
-  geom_bar(stat = "identity", position = "stack") +
-  labs(title = "Production by Stream Order in Yukon",
-       x = "Year",
-       y = "Production",
-       fill = "Stream Order") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+fishcount2015<- 116000 #cumulative passage estimate
+fishcount2016<- 176900 
+fishcount2021<- 124874
 
+fish_across_years <- data_frame(
+  tributary = yuk_2015_all$tribs,
+  Stream_order = yuk_2015_all$stream_order,
+  stream_length = yuk_2015_all$stream_length,
+  year_2015 = yuk_2015_all$rescaled * fishcount2015, 
+  year_2016 = yuk_2016_all$rescaled * fishcount2016,
+  year_2021 = yuk_2021_all$rescaled * fishcount2021
+)
 
-# Pick out only values greater than .7 from normalized
-high_prod_yuk15<- yuk_2015_all %>% 
-  arrange(desc(rescaled)) %>% 
-  filter(normalized > .6) %>%
-  mutate(year = 2015)
+fish_across_years <- mutate(fish_across_years,
+                            avg_mean = (year_2015 + year_2016 + year_2021) / 3)
 
-high_prod_yuk16<- yuk_2016_all %>% 
-  arrange(desc(rescaled)) %>% 
-  filter(normalized > .6) %>%
-  mutate(year = 2016)
+fish_across_years <- fish_across_years %>%
+  rowwise() %>%
+  mutate(sd = sd(c(year_2015, year_2016, year_2021)))
 
-high_prod_yuk17<- yuk_2017_all %>%
-  arrange(desc(rescaled)) %>% 
-  filter(normalized > .6) %>%
-  mutate(year = 2017)
+fish_across_years <- fish_across_years %>%
+  rowwise() %>%
+  mutate(cv = sd/avg_mean)
 
-high_prod_yuk19<- yuk_2019_all %>%
-  arrange(desc(rescaled)) %>% 
-  filter(normalized > .6) %>%
-  mutate(year = 2019)
-
-high_prod_yuk21<- yuk_2021_all %>%
-  arrange(desc(rescaled)) %>% 
-  filter(normalized > .6) %>%
-  mutate(year = 2021)
-
-#Combine all the high production values
-
-high_prod_yuk_all<- rbind(high_prod_yuk15, high_prod_yuk16, high_prod_yuk17, high_prod_yuk19, high_prod_yuk21)
-
-# Plotting
-
-ggplot(high_prod_yuk_all, aes(x = year, y = rescaled, fill = factor(stream_order))) +
-  geom_bar(stat = "identity", position = "stack") +
-  labs(title = "High Production by Stream Order in Yukon",
-       x = "Year",
-       y = "Production",
-       fill = "Stream Order") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+tributary_names<-unique(yuk_across_years$tributary)
 
 
-all_resc_df<- data_frame ( resc_2015 = yuk_2015_all$rescaled,
-                           resc_2016 = yuk_2016_all$rescaled,
-                           resc_2021 = yuk_2021_all$rescaled,)
 
-# Calculate the coefficient of variation across all rows 
-all_resc_df$cv<- apply(all_resc_df, 1, sd)/apply(all_resc_df, 1, mean)
+by_trib_cv<- data_frame(tributary = NA, 
+                        StrOrd = NA, 
+                        mean_cv = NA,
+                        range_cv = NA,
+                        mean_prod = NA, 
+                        range_prod = NA) 
 
-# Calculate the mean production values across all
-all_resc_df$mean_prod <- (all_resc_df$resc_2015 + all_resc_df$resc_2016 + all_resc_df$resc_2021)/3
 
-#normalize the mean production values 
-all_resc_df$norm_mean_prod<- (all_resc_df$mean_prod - min(all_resc_df$mean_prod))/(max(all_resc_df$mean_prod) - min(all_resc_df$mean_prod))
+# Initialize an empty list to store data frames
+trib_data_list <- list()
 
-# Add in stream order and stream length variables 
-all_resc_df$stream_order<- yuk_2015_all$stream_order
-all_resc_df$prop_length<- yuk_2015_all$prop_length
 
-#plot norm mean production vs cv
+###############-----------------------------------------------------------------
 
-all_resc_df <- all_resc_df %>%
-  filter(norm_mean_prod > .4)
 
-ggplot(all_resc_df, aes(x = norm_mean_prod, y = cv, color = stream_order)) +
-  geom_point(alpha = 0.7) +
-  labs(title = "Normalized Mean Production vs Coefficient of Variation in Yukon",
-       x = "Production",
-       y = "CV") +
-  facet_wrap(~stream_order, scales = "free")
+
+for (x in 1:length(tributary_names)) {
+  
+  current_trib <- fish_across_years %>%
+    filter(tributary == tributary_names[x])
+
+  
+  # Initialize an empty list to store data frames for each i
+  trib_data <- list()
+  
+  for (i in 3:9) {
+    
+    current_trib_cv_select <- current_trib %>%
+      filter(Stream_order <= i)
+    
+    
+    # Create a data frame for the current i value
+    by_trib_cv <- data.frame(
+      Tributary = tributary_names[x],
+      StrOrd = i,
+      mean_cv = mean(current_trib_cv_select$cv),
+      mean_prod = mean(current_trib_cv_select$avg_mean)
+    )
+    
+    # Store the data frame in the list
+    trib_data[[i - 2]] <- by_trib_cv  # Store at index corresponding to i value
+    
+  }
+  
+  # Store the list of data frames for the current tributary in the main list
+  trib_data_list[[x]] <- trib_data
+  
+}
 
 
 
 
+names(trib_data_list) <- tributary_names
+combined_data <- bind_rows(unlist(trib_data_list, recursive = FALSE))
+
+plot <- ggplot(combined_data, aes(x = StrOrd, y = mean_cv)) +
+geom_point() +
+  facet_wrap(~ Tributary) +
+  labs(x = "Mean Production", y = "Coefficient of Variation", title = "CV vs Mean Production by Tributary")
