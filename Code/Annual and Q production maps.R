c7b86e6d1399e300e30e52257286faab695f72a0
Ben Makhlouf <makhlobe@gmail.com>
2024-07-16 16:33:30 -0700

    added two new scripts to help clean up the repo

---
+++ Code/Annual and Q production maps.R
@@ -0,0 +1,3 @@
+#### This script will be used to produce annual production maps for all years in the Yukon and Kuskokwim
+#### as well as by quartile within each year. This process will be repeated for a sensitivity threshold .7-.9
+
