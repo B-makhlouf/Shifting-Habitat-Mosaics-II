79c7224ed38d0c9d0b982c7ea1c066d82b0e9b08
Ben Makhlouf <makhlobe@gmail.com>
2024-07-22 10:16:59 -0700

    added code to put the format into tidy for entry into a SQL database

--- Code/Basin Production Maps.R
+++ Code/Basin Production Maps.R
@@ -1,3 +1,19 @@
 #### This script will be used to produce annual production maps for all years in the Yukon and Kuskokwim
 #### as well as by quartile within each year. This process will be repeated for a sensitivity threshold .7-.9
 
+library(here)
+library(tidyverse)
+
+source(here('Code/Helper Functions/Assignment and Map Functions.R'))
+
+Prod<- Basin_prov_assign("Yukon", 2015, .7)
+
+tidy_prod <- Prod %>%
+  gather(key = "Quartile", value = "Production", Q1:Q4) %>%
+  select(-Total)  # Remove the 'Total' column if not needed
+
+# Add a year collumn associated with the year of the data
+tidy_prod$Year <- 2015
+tidy_prod$Watershed <- "Yukon"
+
+
