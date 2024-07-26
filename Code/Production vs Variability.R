c8f70f0e61827de6bbb0373f677a6a6b8857eb9e
Ben Makhlouf <makhlobe@gmail.com>
2024-07-25 20:57:48 -0700

    added scaffolding to make maps of mean and CV

--- Code/Production vs Variability.R
+++ Code/Production vs Variability.R
@@ -53,23 +53,50 @@ write.csv(TotalProd_df, here("Outputs", "Yukon_Prod_CV.csv"), row.names = FALSE)
 #################################################################################
 # Read in the Yukon Shapefile 
 shp_Yukon<- st_read("/Users/benjaminmakhlouf/Desktop/Clean_shapefiles/Yukon_cleaned.shp")
+Yukon_basemap<- st_read("/Users/benjaminmakhlouf/Desktop/Research/isoscapes_new/Yukon/For_Sean/Yuk_Mrg_alb.shp")
 
 ## Add Mean production and CV as attributes to the shapefile 
 shp_Yukon$Mean_Production <- TotalProd_df$Mean
-
-
-
-
+shp_Yukon$CV <- TotalProd_df$CV
 
 # Create maps of distribution of mean production over the whole dataset 
 
+meanProdMap <- ggplot(shp_Yukon) +
+  geom_sf(aes(color = Mean_Production)) +
+  scale_color_gradient2(low = "white", high = "firebrick") +
+  theme_void() +
+  theme(legend.position = "bottom") +
+  labs(title = "Mean Production Across the Yukon Basin", color = "Mean Production")
+
+# Export as a pdf
+ggsave(here("Basin Maps/meanProdMap.pdf"), plot = meanProdMap, width = 10, height = 10, units = "in", dpi = 300)
 
 
 # Create map of the distribution of variation over the dataset 
 
+# Make any NA's in the CV column white
+shp_Yukon$CV[is.na(shp_Yukon$CV)] <- 0
+
+CVmap <- ggplot() +
+  geom_sf(data = Yukon_basemap)+ 
+  geom_sf(data = shp_Yukon, aes(color = CV)) +
+  scale_color_gradient2(low = "dodgerblue4", high = "firebrick") +
+  theme_void() +
+  theme(legend.position = "bottom") +
+  labs(title = "Coefficient of Variation in Production Across the Yukon Basin", color = "Coefficient of Variation")
+
+ggsave(here("Basin Maps/CVmap.pdf"), plot = CVmap, width = 10, height = 10, units = "in", dpi = 300)
+
+
+#Create a scatteplot of mean production vs CV 
+
+scatterplot <- ggplot(TotalProd_df, aes(x = Mean, y = CV)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Mean Production vs Coefficient of Variation", x = "Mean Production", y = "Coefficient of Variation")
 
 
 # Create a bicolor chloropleth map of the coefficient of variation of production values
 
-  
+
 
