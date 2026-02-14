b4180544c33533f1d76b9126538e614b33351292
Ben Makhlouf <makhlobe@gmail.com>
2026-02-14 12:05:29 -0800

    starting to add Yukon to CV stuff but need to fix production stuff first

--- Code/newCV.R
+++ Code/newCV.R
@@ -154,3 +154,83 @@ ggplot() +
        title = "CV of Production by Cluster") +
   theme_minimal(base_size = 14) +
   theme(panel.grid.major.x = element_blank())
+
+
+#################### YUKON
+Yukon_edges <- st_read(here("Data","Spatial Data","AnalysisShapefiles","Yukon_new.shp"))
+
+# Read production data 
+prod2015 <- read.csv(here("Outputs","ProductionData","Yukon","2015_Yukon_Assignment_Results.csv"))
+prod2016 <- read.csv(here("Outputs","ProductionData","Kusko","2018_Kusko_Assignment_Results.csv"))
+prod2018 <- read.csv(here("Outputs","ProductionData","Kusko","2019_Kusko_Assignment_Results.csv"))
+prod2021 <- read.csv(here("Outputs","ProductionData","Kusko","2020_Kusko_Assignment_Results.csv"))
+
+# All escapement 
+library(readxl)
+allEsc <- read_excel(here("Data", "AYKEscapement.xlsx"))
+
+# Pull Kuskokwim Total_Run from each year 
+kusko_esc2017 <- allEsc %>% filter(Year == 2017, River == "Kusko") %>% pull(`Total_Run`)
+kusko_esc2018 <- allEsc %>% filter(Year == 2018, River == "Kusko") %>% pull(`Total_Run`)
+kusko_esc2019 <- allEsc %>% filter(Year == 2019, River == "Kusko") %>% pull(`Total_Run`)
+kusko_esc2020 <- allEsc %>% filter(Year == 2020, River == "Kusko") %>% pull(`Total_Run`)
+kusko_esc2021 <- allEsc %>% filter(Year == 2021, River == "Kusko") %>% pull(`Total_Run`)
+
+kusko_esc_all <- c(kusko_esc2017, kusko_esc2018, kusko_esc2019, kusko_esc2020, kusko_esc2021)
+
+# Multiply assignment_rescale by total escapement
+prod_df <- data.frame(
+  prod_2017 = prod2017$assignment_rescale * kusko_esc2017,
+  prod_2018 = prod2018$assignment_rescale * kusko_esc2018,
+  prod_2019 = prod2019$assignment_rescale * kusko_esc2019,
+  prod_2020 = prod2020$assignment_rescale * kusko_esc2020,
+  prod_2021 = prod2021$assignment_rescale * kusko_esc2021
+)
+
+prod_df$group <- Kusko_edges$upstrm_grp
+prod_df$cluster <- Kusko_edges$cluster
+
+# ============================================================
+# SUMMARIZE & COMPUTE CVs
+# ============================================================
+
+# Summarize by group
+group_summary <- prod_df %>%
+  group_by(group) %>%
+  summarise(
+    total_2017 = sum(prod_2017),
+    total_2018 = sum(prod_2018),
+    total_2019 = sum(prod_2019),
+    total_2020 = sum(prod_2020),
+    total_2021 = sum(prod_2021)
+  ) %>%
+  rowwise() %>%
+  mutate(
+    mean_prod = mean(c(total_2017, total_2018, total_2019, total_2020, total_2021)),
+    sd_prod = sd(c(total_2017, total_2018, total_2019, total_2020, total_2021)),
+    cv_prod = sd_prod / mean_prod
+  ) %>%
+  ungroup() %>%
+  filter(mean_prod >= quantile(mean_prod, prod_quantile_cutoff))
+
+# Summarize by cluster
+cluster_summary <- prod_df %>%
+  group_by(cluster) %>%
+  summarise(
+    total_2017 = sum(prod_2017),
+    total_2018 = sum(prod_2018),
+    total_2019 = sum(prod_2019),
+    total_2020 = sum(prod_2020),
+    total_2021 = sum(prod_2021)
+  ) %>%
+  rowwise() %>%
+  mutate(
+    mean_prod = mean(c(total_2017, total_2018, total_2019, total_2020, total_2021)),
+    sd_prod = sd(c(total_2017, total_2018, total_2019, total_2020, total_2021)),
+    cv_prod = sd_prod / mean_prod
+  ) %>%
+  ungroup() %>%
+  filter(mean_prod >= quantile(mean_prod, prod_quantile_cutoff))
+
+# Basin-wide CV
+basin_cv_kusko <- sd(kusko_esc_all) / mean(kusko_esc_all)
